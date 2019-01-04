793a84b9ad4b14ec6698c83713a3b8e1739529bc
tangxiao <18826097062@163.com>
2019-01-04 14:12:45 +0800

    完成根据城市id或城市名称获取天气数据

--- microservice_weather_basic/src/main/java/com/microservice_weather_basic/service/WeatherDataService.java
+++ microservice_weather_basic/src/main/java/com/microservice_weather_basic/service/WeatherDataService.java
@@ -4,20 +4,23 @@ import com.microservice_weather_basic.pojo.WeatherResponse;
 
 /**
  * 天气数据接口
+ * 
  * @author tangxiao
  *
  */
 public interface WeatherDataService {
 	/**
-	 * 根据城市的id获取天气数据
-	 * @param cityId
+	 * 根据城市id或城市名称拼接地址
+	 * @param city 城市id或城市名称
 	 * @return
 	 */
-	WeatherResponse getDataByCityId(String cityId);
+	WeatherResponse getData(String city);
+
 	/**
-	 * 根据城市的名称获取天气数据
-	 * @param cityName
+	 * 获取天气数据
+	 * 
+	 * @param url 地址
 	 * @return
 	 */
-	WeatherResponse getDataByCityName(String cityName);
+	WeatherResponse doGetWeahter(String url);
 }
--- microservice_weather_basic/src/main/java/com/microservice_weather_basic/service/impl/WeatherDataServiceImpl.java
+++ microservice_weather_basic/src/main/java/com/microservice_weather_basic/service/impl/WeatherDataServiceImpl.java
@@ -1,21 +1,58 @@
 package com.microservice_weather_basic.service.impl;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microservice_weather_basic.pojo.WeatherResponse;
 import com.microservice_weather_basic.service.WeatherDataService;
-
+import com.microservice_weather_basic.util.NumberDetectionUtil;
+@Service
 public class WeatherDataServiceImpl implements WeatherDataService {
 	private static final String WEATHER_URI = "http://wthrcdn.etouch.cn/weather_mini?";
-	private String uri;
+	private String url;
+	private WeatherResponse wr;
+	@Autowired
+	private RestTemplate resttemplate;
+	
 	@Override
-	public WeatherResponse getDataByCityId(String cityId) {
-		uri = WEATHER_URI + "citykey=" + cityId;
-		return null;
+	public WeatherResponse getData(String city) {
+		if(NumberDetectionUtil.judgeNumber(city)) {
+			url = WEATHER_URI + "citykey=" + city;
+			return doGetWeahter(url);
+		}
+		url = WEATHER_URI + "city=" + city;
+		return doGetWeahter(url);
 	}
+	
 
 	@Override
-	public WeatherResponse getDataByCityName(String cityName) {
-		uri = WEATHER_URI + "city=" + cityName;
-		return null;
+	public WeatherResponse doGetWeahter(String url) {
+		// 发送一个HTTP GET请求，返回的ResponseEntity包含了响应体所映射成的对象.这里返回一个json字符串
+		ResponseEntity<String> respString = resttemplate.getForEntity(url, String.class);
+
+		// json处理框架
+		ObjectMapper om = new ObjectMapper();
+
+		// 存储json字符串
+		String strBody = null;
+
+		// 通过状态码来判断ResponseEntity是否访问成功
+		if (respString.getStatusCodeValue() == 200) {
+			strBody = respString.getBody();
+		}
+
+		// 把json字符串转为对象
+		try {
+			wr = om.readValue(strBody, WeatherResponse.class);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return wr;
 	}
 
 }
