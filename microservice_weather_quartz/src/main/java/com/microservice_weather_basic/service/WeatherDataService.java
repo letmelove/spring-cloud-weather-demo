d99d33342dffd60ec7375e15d68a7a291323fe18
tangxiao <18826097062@163.com>
2019-01-07 16:37:52 +0800

    使用定时器，每隔30分钟自动获取天气数据到redis中

---
+++ microservice_weather_quartz/src/main/java/com/microservice_weather_basic/service/WeatherDataService.java
@@ -0,0 +1,30 @@
+package com.microservice_weather_basic.service;
+
+import com.microservice_weather_basic.pojo.WeatherResponse;
+
+/**
+ * 天气数据接口
+ * 
+ * @author tangxiao
+ *
+ */
+public interface WeatherDataService {
+	/**
+	 * 根据城市id或城市名称拼接地址
+	 * @param city 城市id或城市名称
+	 * @return
+	 */
+	WeatherResponse getData(String city);
+
+	/**
+	 * 获取天气数据
+	 * 
+	 * @param url 地址
+	 * @return
+	 */
+	WeatherResponse doGetWeahter(String url);
+	/**
+	 * 根据城市同步天气
+	 */
+	void synDateByCity(String cityId);
+}
