ada9acc2a122f90dd4352f50246de8b56924da4a
tangxiao <18826097062@163.com>
2019-01-14 09:57:00 +0800

    添加注释

--- microservice-weather-report-server/src/main/java/com/microservice_weather_basic/App.java
+++ microservice-weather-report-server/src/main/java/com/microservice_weather_basic/App.java
@@ -24,7 +24,7 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
-	
+	//springboot 版本如果是2.0则需要添加 ServletRegistrationBean 因为springboot的默认路径不是 "/hystrix.stream"
 	@Bean
     public ServletRegistrationBean getServlet() {
         HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
